6dab4bd004f37309e78c7ea6991a1fa4d493cf8b
bornav <borna.vincek1@gmail.com>
2025-05-17 17:10:40 +0200

    removal of wireguard mesh and added the submodule

---
+++ nix/lighthouse/vxlan.nix
@@ -0,0 +1,20 @@
+{ config, lib, inputs, vars, pkgs, pkgs-unstable, ... }:
+{
+  # systemd.network = {
+  #   netdevs= {
+  #   "vx1" = {
+  #       netdevConfig = {
+  #         Name = "vx1";
+  #         Kind = "vxlan";
+  #       };
+
+  #       vxlanConfig = {
+  #         VNI = 16;
+  #         Remote = "10.99.10.14";
+  #         Local = "10.99.10.254";
+  #         Independent = true;
+  #       };
+  #     };
+  #   };
+  # };
+}
