34e980de7f6589adc95cc8a59e6ca874c7d8b9a2
bornav <borna.vincek1@gmail.com>
2025-08-07 09:26:27 +0200

    added local node custom flake.lock ability

---
+++ nix/local_flake/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A very basic flake";
+
+  inputs = {                                                              # References Used by Flake
+      # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";                     # Stable Nix Packages (Default)
+      # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+      nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";         # Unstable Nix Packages
+      nixpkgs-master.url = "github:nixos/nixpkgs/master"; 
+      # nixpkgs-bornav.url = "github:bornav/nixpkgs/feat/headscale-allow-external-config"; 
+      nixpkgs-bornav.url = "git+file:///home/bocmo/git/nixpkgs"; 
+      nixos-hardware.url = "github:NixOS/nixos-hardware/master"; #https://github.com/NixOS/nixos-hardware/tree/master
+      home-manager = {url = "github:nix-community/home-manager";
+                      # inputs.nixpkgs.follows = "nixpkgs-unstable";
+                      };
+      nix-flatpak.url = "github:gmodena/nix-flatpak";
+      nur.url = "github:nix-community/NUR";
+      disko = {url = "github:nix-community/disko";
+              #  inputs.nixpkgs.follows = "nixpkgs-unstable";
+               };
+      nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
+    };
+  outputs = { self, nur, nixpkgs-unstable, nixpkgs-stable, nixpkgs-master, home-manager, nixos-hardware, nix-flatpak, disko, ... } @ inputs:   # Function telling flake which inputs to use
+	let
+		vars = {                                                              # Variables Used In Flake
+			user = "nix";
+			location = "$HOME/.flake";
+			terminal = "alacritty";
+			editor = "vim";
+      stateVersion = "25.05";
+		};
+    inherit (self) outputs;
+	in {
+		nixosConfigurations = (
+			import ../nix {
+			inherit inputs outputs self vars;   # Inherit inputs
+			}
+		);
+		# homeConfigurations = (
+		# 	import ./nix {
+		# 	inherit (nixpkgs) lib;
+		# 	inherit inputs nixpkgs nixpkgs-unstable home-manager vars;
+		# 	}
+		# );
+	};
+}
