5aeee792eadd4a8e6e74b4953a7bbef62d2136ea
bornav <borna.vincek1@gmail.com>
2025-12-23 18:45:58 +0100

    IMPO removing deployment as legacy and unused

--- deployment/terraform/ansible/test.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-#SSH KEY
-#ansible-playbook ./playbook/purge-net-blockers.yaml --user bocmo --ask-pass --ask-become-pass -i ./inventory/hosts
-
-if [[ $BOOTSTRAP == true ]]; then
-    cd ../../
-    chmod +x ./kubernetes/bootstrap/bootstrap.sh
-    ./kubernetes/bootstrap/bootstrap.sh
-fi
