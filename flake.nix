eee0a3798378a137800b98bb3f17a0830335cc8d
bornav <borna.vincek1@gmail.com>
2026-04-11 11:40:45 +0200

    pit

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     # nixpkgs-bornav.url = "github:bornav/nixpkgs/feat/headscale-allow-external-config";
     nixpkgs-bornav.url = "github:bornav/nixpkgs/test-rebase";
+    nixpkgs-test.url = "github:bornav/nixpkgs/noissue-testing";
     # nixpkgs-bornav.url = "git+file:///home/user/git/nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master"; #https://github.com/NixOS/nixos-hardware/tree/master
     home-manager = {
@@ -24,6 +25,7 @@
     };
     nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
     sops-nix.url = "github:Mic92/sops-nix";
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
   outputs = {self, ...} @ inputs:
   # Function telling flake which inputs to use
@@ -43,12 +45,16 @@
         inherit inputs outputs self vars; # Inherit inputs
       }
     );
-    hydraJobs = import ./hydra.nix {inherit inputs outputs;};
+    deploy = import ./nix/deploy.nix {inherit inputs outputs self vars;};
+    # hydraJobs = import ./hydra.nix {inherit inputs outputs;};
     # homeConfigurations = (
     # 	import ./nix {
     # 	inherit (nixpkgs) lib;
     # 	inherit inputs nixpkgs nixpkgs-unstable home-manager vars;
     # 	}
     # );
+    #
+
+    # checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
   };
 }
