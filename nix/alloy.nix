5794acbd86eb2e28eed71ed8e7f899041b89a6b4
bornav <borna.vincek1@gmail.com>
2026-06-14 20:44:46 +0200

    alloy temp

--- nix/alloy.nix
+++ nix/alloy.nix
@@ -1,4 +1,8 @@
-{lib, pkgs-bornav-test, ...}: let
+{
+  lib,
+  pkgs-bornav-test,
+  ...
+}: let
   # docker_socket = "unix:///var/run/docker.sock";
   docker_socket = "unix:///var/run/podman/podman.sock";
   prometheus_ingest = "http://10.129.16.104:9090/api/v1/write";
@@ -15,33 +19,51 @@ in {
   services.alloy.enable = true;
   services.alloy.extraFlags = ["--disable-reporting"]; # this removes the anon usage statistics
   environment.etc."alloy/config.alloy".text = lib.mkForce ''
-    prometheus.remote_write "local" {
-      endpoint {
-        url = "${prometheus_ingest}"
-      }
-    }
-    loki.write "local" {
-      endpoint {
-        url = "${loki_ingest}"
-      }
-    }
     otelcol.exporter.otlp "local" {
       client {
         endpoint = "${otel_ingest}"
+        tls {
+           insecure = true
+        }
+      }
+      sending_queue {
+        enabled    = true
+        num_consumers = 10      // parallel sender goroutines (default 10)
+        queue_size    = 10000   // raise from default 1000
+      }
+      retry_on_failure {
+        enabled          = true
+        initial_interval = "5s"
+        max_interval     = "30s"
+        max_elapsed_time = "300s"
       }
     }
-    pyroscope.write "local" {
-      endpoint {
-        url = "${pytoscope_ingest}"
+    otelcol.processor.batch "default" {
+      timeout          = "5s"    // flush at least every 5s
+      send_batch_size  = 1000    // or when 1000 items accumulate
+      send_batch_max_size = 2000
+
+      output {
+        logs    = [otelcol.exporter.otlp.local.input]
+        metrics = [otelcol.exporter.otlp.local.input]
+        traces  = [otelcol.exporter.otlp.local.input]
       }
     }
-    prometheus.scrape "linux_node" {
-      targets = prometheus.exporter.unix.node.targets
-      forward_to = [
-        prometheus.remote_write.local.receiver,
-      ]
+    otelcol.receiver.loki "default" {
+      output {
+        logs = [otelcol.processor.batch.default.input]
+      }
+    }
+    otelcol.receiver.prometheus "default" {
+      output {
+        metrics = [otelcol.processor.batch.default.input]
+      }
     }
     prometheus.exporter.unix "node" {}
+    prometheus.scrape "linux_node" {
+      targets    = prometheus.exporter.unix.node.targets
+      forward_to = [otelcol.receiver.prometheus.default.receiver]
+    }
     loki.relabel "journal" {
       forward_to = []
       rule {
@@ -66,9 +88,7 @@ in {
       }
     }
     loki.source.journal "read" {
-      forward_to = [
-        loki.write.local.receiver,
-      ]
+      forward_to    = [otelcol.receiver.loki.default.receiver]
       relabel_rules = loki.relabel.journal.rules
       labels = {
         "job" = "integrations/node_exporter",
@@ -78,39 +98,37 @@ in {
       host = "${docker_socket}"
     }
     discovery.relabel "logs_integrations_docker" {
-        targets = []
-        rule {
-            target_label = "job"
-            replacement  = "integrations/docker"
-        }
-        rule {
-            target_label = "instance"
-            replacement  = constants.hostname
-        }
-        rule {
-            source_labels = ["__meta_docker_container_name"]
-            regex         = "/(.*)"
-            target_label  = "container"
-        }
-        rule {
-            source_labels = ["__meta_docker_container_log_stream"]
-            target_label  = "stream"
-        }
+      targets = []
+      rule {
+        target_label = "job"
+        replacement  = "integrations/docker"
+      }
+      rule {
+        target_label = "instance"
+        replacement  = constants.hostname
+      }
+      rule {
+        source_labels = ["__meta_docker_container_name"]
+        regex         = "/(.*)"
+        target_label  = "container"
+      }
+      rule {
+        source_labels = ["__meta_docker_container_log_stream"]
+        target_label  = "stream"
+      }
     }
     loki.source.docker "default" {
-      host = "${docker_socket}"
-      targets = discovery.docker.linux.targets
+      host          = "${docker_socket}"
+      targets       = discovery.docker.linux.targets
       relabel_rules = discovery.relabel.logs_integrations_docker.rules
-      labels = {}
-      forward_to = [
-        loki.write.local.receiver,
-      ]
+      labels        = {}
+      forward_to    = [otelcol.receiver.loki.default.receiver]
     }
 
   '';
 
   systemd.services.beyla = {
-    after = [ "network.target"];
+    after = ["network.target"];
     # environment = { BEYLA_NETWORK_PRINT_FLOWS = "true";};
     serviceConfig = {
       ExecStart = "${pkgs-bornav-test.beyla}/bin/beyla -config /etc/beyla/config.yaml";
@@ -118,7 +136,7 @@ in {
       RestartSec = "5s";
       RemainAfterExit = true;
     };
-    wantedBy = [ "multi-user.target" ];
+    wantedBy = ["multi-user.target"];
   };
   environment.systemPackages = [pkgs-bornav-test.beyla];
   environment.etc."beyla/config.yaml".text = lib.mkForce ''
